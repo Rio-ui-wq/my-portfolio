3162bcd188ef00d5f5a54de425c6fe9079b6462b
Rio <r.flower.m@icloud.com>
2026-08-29 20:06:18 +0900

    Firebase設定を環境変数に移行、README更新、未使用ファイル削除

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2zrqKga-cU924uS0KaSw2ncT7kHgo3Gs",
-  authDomain: "my-portfolio-884af.firebaseapp.com",
-  projectId: "my-portfolio-884af",
-  storageBucket: "my-portfolio-884af.firebasestorage.app",
-  messagingSenderId: "684717750812",
-  appId: "1:684717750812:web:4c6029abd484a1bec83dd9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
